24c96547ddd73eebb6bff449e4c342bf78ca7faa
hadeelshahin <sha.99hadeel@gmail.com>
2024-04-05 11:02:21 +0300

    Updated project

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const productsController = require("../controllers/products");
+const shopController = require("../controllers/admin");
 
 
 
-router.get("/",productsController.getProducts);
+router.get("/",shopController.getProducts); //statring page
+//router.get("/products", productsController.getProducts);
+//router.get('/cart');
+//router.get('checkout');
+
 
 module.exports = router;
